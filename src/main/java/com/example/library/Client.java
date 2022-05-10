c8f5b802e414564e111c5960c1e033e97df785ea
khan513 <abylaikhan513@gmail.com>
2022-05-10 21:43:25 +0600

    minor change

--- src/main/java/com/example/library/Client.java
+++ src/main/java/com/example/library/Client.java
@@ -3,16 +3,18 @@ package com.example.library;
 import com.example.library.model.User;
 import com.example.library.net.Api;
 import com.example.library.net.TcpApi;
+import com.example.library.util.Alert;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.Socket;
+
+import static com.example.library.util.Alert.showAlert;
 
 public class Client extends Application {
     public static User user;
@@ -29,11 +31,7 @@ public class Client extends Application {
             socket = new Socket("127.0.0.1", 1234);
             api = new TcpApi();
         } catch (IOException e) {
-            Alert alert = new Alert(Alert.AlertType.WARNING, "Could not connect to the server, please try again later");
-            alert.setTitle("No connection with server");
-            alert.setWidth(300);
-            alert.setHeight(300);
-            alert.show();
+            showAlert(Alert.AlertType.WARNING, "Could not connect to the server, please try again later"));
             e.printStackTrace();
         }
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login-view.fxml"));
